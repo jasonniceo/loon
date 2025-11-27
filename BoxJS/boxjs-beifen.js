ba61e2fc5f9bef55650ac5b0d7fd7caee73e386c
蔚蓝色星辰 <46687388+jasonniceo@users.noreply.github.com>
2025-11-28 00:54:54 +0800

    Update boxjs-beifen.js

--- BoxJS/boxjs-beifen.js
+++ BoxJS/boxjs-beifen.js
@@ -4,10 +4,10 @@
 
 [Script]
 # 备份
-cron "55 23 * * *" script-path=https://raw.githubusercontent.com/jasonniceo/loon/refs/heads/main/BoxJS/backup.js, timeout=120, tag=BoxJS gist备份
+cron "55 23 * * *" script-path=https://raw.githubusercontent.com/jasonniceo/loon/refs/heads/main/BoxJS/backup.js?token=GHSAT0AAAAAADQFF7FYVTDWIUXFR3Z73HX42JIQFSA, timeout=120, tag=BoxJS gist备份
 
 # 缓存历史记录
-cron "55 23 * * *" script-path=https://raw.githubusercontent.com/jasonniceo/loon/refs/heads/main/BoxJS/commit.js, timeout=120, tag=BoxJS gist缓存历史记录
+cron "55 23 * * *" script-path=https://raw.githubusercontent.com/jasonniceo/loon/refs/heads/main/BoxJS/commit.js?token=GHSAT0AAAAAADQFF7FZNAJUOHBZHDWFOQKQ2JIQGIQ, timeout=120, tag=BoxJS gist缓存历史记录
 
 # 恢复
-cron "55 23 * * *" script-path=https://raw.githubusercontent.com/jasonniceo/loon/refs/heads/main/BoxJS/restore.js, timeout=120, tag=BoxJS gist恢复
+cron "55 23 * * *" script-path=https://raw.githubusercontent.com/jasonniceo/loon/refs/heads/main/BoxJS/restore.js?token=GHSAT0AAAAAADQFF7FY4EFEFA5WAGS2MX7M2JIQGWA, timeout=120, tag=BoxJS gist恢复
