1654e49bdd3de6011c8991efb9f2e34d12b77267
蔚蓝色星辰 <46687388+jasonniceo@users.noreply.github.com>
2025-11-28 11:18:43 +0800

    Update and rename boxjs-beifen.js to boxjs备份和恢复.js

--- BoxJS/boxjs备份和恢复.js
+++ BoxJS/boxjs备份和恢复.js
@@ -4,10 +4,10 @@
 
 [Script]
 # 备份
-cron "55 23 * * *" script-path=https://raw.githubusercontent.com/jasonniceo/loon/refs/heads/main/BoxJS/backup.js, timeout=120, tag=BoxJS gist备份
+cron "40 23 * * *" script-path=https://raw.githubusercontent.com/jasonniceo/loon/refs/heads/main/BoxJS/backup.js, timeout=120, tag=BoxJS gist备份
 
 # 缓存历史记录
-cron "55 23 * * *" script-path=https://raw.githubusercontent.com/jasonniceo/loon/refs/heads/main/BoxJS/commit.js, timeout=120, tag=BoxJS gist缓存历史记录
+cron "50 23 * * *" script-path=https://raw.githubusercontent.com/jasonniceo/loon/refs/heads/main/BoxJS/commit.js, timeout=120, tag=BoxJS gist缓存历史记录
 
 # 恢复
 cron "55 23 * * *" script-path=https://raw.githubusercontent.com/jasonniceo/loon/refs/heads/main/BoxJS/restore.js, timeout=120, tag=BoxJS gist恢复
